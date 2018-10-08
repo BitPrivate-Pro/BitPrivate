22e76bc86796a36dd930428b668b48f8a98e4001
BitPrivate-Pro <43960301+BitPrivate-Pro@users.noreply.github.com>
2018-10-08 23:56:27 +0800

    Update HttpServer.cpp

--- src/Rpc/HttpServer.cpp
+++ src/Rpc/HttpServer.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2014-2016 XDN developers
 // Copyright (c) 2016-2018 Karbowanec developers
-// Copyright (c) 2018 Brazukcoin developers
+// Copyright (c) 2018 BitPrivate developers
 //
 // This file is part of Bytecoin.
 //
