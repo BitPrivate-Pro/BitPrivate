2d13d4f6abb76626fa030405aea7141d7cf0926f
BitPrivate-Pro <muhammadasyrafzaffarullah88@gmail.com>
2018-10-08 21:38:33 +0800

    First Commit

---
+++ build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/connecthostport.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/connecthostport.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/igd_desc_parse.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/igd_desc_parse.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/minisoap.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/minisoap.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/minissdpc.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/minissdpc.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/miniupnpc.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/miniupnpc.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/miniwget.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/miniwget.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/minixml.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/minixml.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/portlistingparse.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/portlistingparse.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/receivedata.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/receivedata.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/upnpc.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/upnpc.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/upnpcommands.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/upnpcommands.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/upnperrors.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/upnperrors.c.o"
+  "/home/megarootz/Desktop/BitPrivate/external/miniupnpc/upnpreplyparse.c" "/home/megarootz/Desktop/BitPrivate/build/release/external/miniupnpc/CMakeFiles/upnpc-static.dir/upnpreplyparse.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "MINIUPNPC_SET_SOCKET_TIMEOUT"
+  "_BSD_SOURCE"
+  "_POSIX_C_SOURCE=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/release/src/CMakeFiles/System.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/megarootz/Desktop/BitPrivate/src/Platform/Linux/System/Dispatcher.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/Dispatcher.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/Platform/Linux/System/ErrorMessage.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/ErrorMessage.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/Platform/Linux/System/Ipv4Resolver.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/Ipv4Resolver.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/Platform/Linux/System/TcpConnection.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/TcpConnection.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/Platform/Linux/System/TcpConnector.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/TcpConnector.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/Platform/Linux/System/TcpListener.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/TcpListener.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/Platform/Linux/System/Timer.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/Timer.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/Platform/Posix/System/MemoryMappedFile.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/Platform/Posix/System/MemoryMappedFile.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/System/ContextGroup.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/System/ContextGroup.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/System/ContextGroupTimeout.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/System/ContextGroupTimeout.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/System/Event.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/System/Event.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/System/EventLock.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/System/EventLock.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/System/InterruptedException.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/System/InterruptedException.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/System/Ipv4Address.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/System/Ipv4Address.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/System/RemoteEventLock.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/System/RemoteEventLock.cpp.o"
+  "/home/megarootz/Desktop/BitPrivate/src/System/TcpStream.cpp" "/home/megarootz/Desktop/BitPrivate/build/release/src/CMakeFiles/System.dir/System/TcpStream.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
