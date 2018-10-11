d56a83d149d060b267c3ca28bc9709195867db2a
BitPrivate-Pro <43960301+BitPrivate-Pro@users.noreply.github.com>
2018-10-11 18:54:44 +0800

    Update NodeRpcProxy.h

--- src/NodeRpcProxy/NodeRpcProxy.h
+++ src/NodeRpcProxy/NodeRpcProxy.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2016-2017 The Karbowanec developers
-// Copyright (c) 2018 The Brazukcoin developers
+// Copyright (c) 2018 The BitPrivate developers
 //
 // This file is part of Bytecoin.
 //
