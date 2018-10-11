7c9fe519f4bfee414da4ba211209b2434c0b44ee
BitPrivate-Pro <43960301+BitPrivate-Pro@users.noreply.github.com>
2018-10-11 19:02:34 +0800

    Update Currency.cpp

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2016-2018  zawy12
 // Copyright (c) 2016-2018, The Karbowanec developers
-// Copyright (c) 2018, The Brazukcoin developers
+// Copyright (c) 2018, The BitPrivate developers
 //
 // This file is part of Bytecoin.
 //
